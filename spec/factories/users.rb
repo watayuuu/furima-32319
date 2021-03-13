adffd8cda84b4b19e2e4aa4c13f46a52b53953a1
watayuuu <www4869462@gmail.com>
2021-03-13 14:16:55 +0900

    ユーザー管理機能の実装１

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {'手巣戸'}
+    first_name            {'太郎'}
+    last_name_kana        {'テスト'}
+    first_name_kana       {'タロウ'}
+    birthday              {'2021-01-01'}
+  end
+end
