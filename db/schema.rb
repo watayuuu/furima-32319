45b1872f50255b129531ba3f146bb467d13ac131
watayuuu <www4869462@gmail.com>
2021-03-28 12:46:39 +0900

    商品一覧機能の修正１

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_040858) do
+ActiveRecord::Schema.define(version: 2021_03_28_022057) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2021_03_14_040858) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "managements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_managements_on_item_id"
+    t.index ["user_id"], name: "index_managements_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -68,4 +77,6 @@ ActiveRecord::Schema.define(version: 2021_03_14_040858) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "managements", "items"
+  add_foreign_key "managements", "users"
 end
