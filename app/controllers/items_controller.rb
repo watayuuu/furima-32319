4cdc4a9829c11753664c8fe1a92dc4610e12e96f
watayuuu <www4869462@gmail.com>
2021-03-29 18:06:46 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    
+  end
+
+  def edit
+  
+    
+  end
   private
 
   def item_params
